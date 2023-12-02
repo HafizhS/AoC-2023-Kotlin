fe9923bcfafb86b6d7db33c8bdd01f22e55bfb2b
HafizhS <methafizh30@gmail.com>
2023-12-02 09:40:18 +0700

    Day 01 Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,64 @@
 fun main() {
     fun part1(input: List<String>): Int {
         return input.map {
-            val digitOnly = it.replace("\\D".toRegex(),"")
+            val digitOnly = it.replace("\\D".toRegex(), "")
             val fd = digitOnly[0].toString()
-            val ld = digitOnly[digitOnly.length-1].toString()
-            return@map (fd+ld).toInt()
+            val ld = digitOnly[digitOnly.length - 1].toString()
+            return@map (fd + ld).toInt()
         }.sum()
     }
 
+    fun part2(input: List<String>): Int {
+        val digitDict = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9
+        )
+
+        var sum = 0;
+
+        val result = input.map { line ->
+            var result = line
+            var i = 0;
+//            println(line)
+            while (i <= result.length) {
+                for ((letter, number) in digitDict) {
+                    val endIndex = (i + letter.length).coerceAtMost(result.length)
+                    val substr = result.substring(i, endIndex)
+//                    println("$substr == $letter")
+                    if (substr == letter) {
+                        result = result.replace(substr, number.toString() + substr.last())
+//                        println("true $result")
+                        break
+                    }
+                }
+                i++
+            }
+            val digitOnly = result.replace("\\D".toRegex(), "")
+            val fd = digitOnly[0].toString()
+            val ld = digitOnly[digitOnly.length - 1].toString()
+//            println((fd + ld).toInt())
+            return@map (fd + ld).toInt()
+        }
+        sum.println()
+        return result.sum()
+    }
+
+    println("Test Input:")
     val testInput = readInput("Day01_test")
     println(part1(testInput))
 
+    val testInputPart2 = readInput("Day01_test2")
+    println(part2(testInputPart2))
+
+    println("Input:")
     val input = readInput("Day01")
     part1(input).println()
+    part2(input).println()
 }
