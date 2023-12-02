b8141a25872d60f41af8a1b6665a44567f674b34
HafizhS <methafizh30@gmail.com>
2023-12-02 22:29:13 +0700

    Day 02 Part 1

---
+++ src/Day02.kt
@@ -0,0 +1,82 @@
+import kotlin.contracts.contract
+
+data class Game(
+    val id: Int,
+    val gameSets: List<GameSets>
+) {
+    companion object {
+        fun parse(raw: String): Game {
+            val data = raw.split(": ")
+            return Game(
+                id = data[0].split(" ")[1].toInt(),
+                gameSets = GameSets.parse(data[1])
+            )
+        }
+    }
+}
+
+data class GameSets(
+    val setNumber: Int = 0,
+    val red: Int = 0,
+    val green: Int = 0,
+    val blue: Int = 0
+) {
+    companion object {
+        fun parse(raw: String): List<GameSets> {
+            val data = raw.replace(" ", "").split(";")
+            return data.indices.map { i ->
+                val colorList = listOf("red", "green", "blue").map { color ->
+                    var colorValue = 0
+                    data[i].split(",").forEach {
+                        if (it.replace("\\d".toRegex(), "") == color) {
+                            colorValue = it.replace("\\D".toRegex(), "").toInt()
+                        }
+                    }
+                    colorValue
+                }
+                GameSets(
+                    setNumber = i,
+                    red = colorList[0],
+                    green = colorList[1],
+                    blue = colorList[2]
+                )
+            }
+        }
+    }
+}
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        val maxRed = 12
+        val maxGreen = 13
+        val maxBlue = 14
+
+        val result = input.map {
+            Game.parse(it).let { game ->
+                game.gameSets.forEach {set ->
+                    if (set.red > maxRed) return@map 0
+                    if (set.green > maxGreen) return@map 0
+                    if (set.blue > maxBlue) return@map 0
+                }
+
+                return@map game.id
+            }
+        }
+        result.println()
+        return result.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    println("Test Input:")
+    val testInput = readInput("Day02_test")
+    println(part1(testInput))
+
+    println("\nInput:")
+    val input = readInput("Day02")
+    part1(input).println()
+
+}
